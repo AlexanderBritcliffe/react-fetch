117216642e11d2ccec5457f321887c7a486f2f93
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2019-11-05 17:06:58 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      beers: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch ('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(parsedJSON => parsedJSON.map(beer => ({
+      name: `${beer.name}`,
+      tagline: `${beer.tagline}`,
+      abv: `${beer.abv}`,
+      ibu: `${beer.ibu}`,
+    })))
+    .then(beers => this.setState({
+      beers,
+      isLoading: false
+    }))
+    .catch(error => console.log("parsing failed", error))
+    }
+
   render() {
+    const {isLoading, beers} = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="whole-page">
+        <header>The Best Beers</header>
+        <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+          <div className="beer-info">
+            {
+              !isLoading && beers.length > 0 ? beers.map(beer => {
+                const {name, tagline, abv, ibu} = beer;
+                return <div key={beer} title={name}>
+                  <p className="name-line">{name} <button onclick="likeClicked()" className="like-button">Like</button></p>
+                  <p className="tagline-line"> {tagline}</p>
+                  <p className="abv-line"> {abv}</p>
+                  <p className="ibu-line"> {ibu}</p>
+                  <br></br>
+                  </div>
+              }) : null
+            }
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
